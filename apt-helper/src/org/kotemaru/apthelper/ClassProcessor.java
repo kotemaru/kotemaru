b8f658480dc611009392c3323e83dc3b861900dc
kotemaru@kotemaru.org <kotemaru@kotemaru.org@007fbaa9-8b8e-5bb4-69cf-ca3c05551f93>
2013-03-02 07:07:42 +0000

    prottype

--- apt-helper/src/org/kotemaru/apthelper/ClassProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.kotemaru.apthelper;
-
-import com.sun.mirror.declaration.TypeDeclaration;
-
-public interface ClassProcessor {
-	boolean processClass(TypeDeclaration classDecl) throws Exception;
-}
