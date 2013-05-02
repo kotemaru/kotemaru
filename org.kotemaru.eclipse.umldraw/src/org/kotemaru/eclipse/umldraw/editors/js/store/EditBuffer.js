7e5c03f73d8035715d992ac19f834b5047f25558
kotemaru@kotemaru.org <kotemaru@kotemaru.org@007fbaa9-8b8e-5bb4-69cf-ca3c05551f93>
2013-05-02 06:58:05 +0000

    UNDO,REDO OK

--- org.kotemaru.eclipse.umldraw/src/org/kotemaru/eclipse/umldraw/editors/js/store/EditBuffer.js
+++ org.kotemaru.eclipse.umldraw/src/org/kotemaru/eclipse/umldraw/editors/js/store/EditBuffer.js
@@ -27,24 +27,32 @@ function EditBuffer(){this.initialize.apply(this, arguments)};
 		Store.paste(copyBuff, xx,yy);
 	}
 	
+	_class.init = function() {
+		curData = Store.save(Canvas.getItems());
+		undoBuff.length = 0;
+		redoBuff.length = 0;
+	}
+
 	_class.backup = function(data) {
 		if (data) curData = data;
 		undoBuff.push(curData);
 		redoBuff.length = 0;
 		curData = Store.save(Canvas.getItems());
-		Eclipse.fireEvent("change");
+		Eclipse.fireEvent("change,"+undoBuff.length);
 	}
 	_class.undo = function() {
 		if (undoBuff.length == 0) return;
 		redoBuff.push(curData);
 		curData = undoBuff.pop();
 		Store.load(curData);
+		Eclipse.fireEvent("change,"+undoBuff.length);
 	}
 	_class.redo = function() {
 		if (redoBuff.length == 0) return;
 		undoBuff.push(curData);
 		curData = redoBuff.pop();
 		Store.load(curData);
+		Eclipse.fireEvent("change,"+undoBuff.length);
 	}
 	
 })(EditBuffer);
