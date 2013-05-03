ca3b8b8c61d593723876ff2c661faabfee9e9a42
kotemaru@kotemaru.org <kotemaru@kotemaru.org@007fbaa9-8b8e-5bb4-69cf-ca3c05551f93>
2013-05-03 03:17:47 +0000

    point out bounds ok

--- org.kotemaru.eclipse.umldraw/src/org/kotemaru/eclipse/umldraw/editors/js/Coor.js
+++ org.kotemaru.eclipse.umldraw/src/org/kotemaru/eclipse/umldraw/editors/js/Coor.js
@@ -7,7 +7,9 @@ function Coor(){this.initialize.apply(this, arguments)};
 		_origin : {type: "Point", value:null},
 		_origin2: {type: "Point", value:null},
 		_x      : {type: "number", value:0},
-		_y      : {type: "number", value:0}
+		_y      : {type: "number", value:0},
+		mode_x  : {type: "number", value:0},
+		mode_y  : {type: "number", value:0},
 	};
 	
 	var idCount = 1;
@@ -40,33 +42,38 @@ function Coor(){this.initialize.apply(this, arguments)};
 		this.y(yy);
 	}
 	_class.prototype.x = function(v) {
-		if (v) {
-			if (this._origin) {
-				var x1 = this._origin.x();
-				if (this._origin2) {
-					var x2 = this._origin2.x();
-					var per = (v - x1) / (x2-x1);
-					
-					if (-1.0 <= per && per<= 1.0) {
-						this._x = per;
-					} else {
-						var ox = x1 + (x2-x1)/2;
-						this._x = v - ox;
-					}
-				} else {
-					this._x = v - x1 ;
-				}
-			} else {
-				this._x = v;
-			}
-			return this;
-		}
-		
+		if (v) return this.putX(v);
+
 		this.checkRemove();
 		var xx = this.calcX();
 		if (xx<0) xx = 0;
 		if (xx>Canvas.width()) xx = Canvas.width;
 		return xx;
+	}
+	_class.prototype.putX = function(v) {
+		if (this._origin) {
+			var x1 = this._origin.x();
+			if (this._origin2) {
+				var x2 = this._origin2.x();
+				var w = x2-x1;
+				var per = (v - x1) / w;
+				
+				if (0.0 <= per && per<= 1.0) {
+					this.mode_x = 2; // FLOATING-IN-BOUNDS
+					this._x = per;
+				} else {
+					this.mode_x = 3; // FLOATING-OUT-BOUNDS
+					this._x = v - (x1 + w/2);
+				}
+			} else {
+				this.mode_x = 1; // RELATIVE
+				this._x = v - x1 ;
+			}
+		} else {
+			this.mode_x = 0; // ABSOLUTE
+			this._x = v;
+		}
+		return this;
 	}
 	_class.prototype.calcX = function() {
 		if (this._origin) {
@@ -76,52 +83,75 @@ function Coor(){this.initialize.apply(this, arguments)};
 				var w = x2 - x1;
 				var per = this._x;
 				
-				if (-1.0 <= per && per <= 1.0) {
-					return x1 + w * per;
+				if (0.0 <= per && per <= 1.0) {
+					return x1 + w * per; // FLOATING-IN-BOUNDS
 				} else {
-					return x1 + w/2 + this._x;
+					return x1 + w/2 + this._x; // FLOATING-OUT-BOUNDS
 				}
 			} else {
-				return x1 + this._x;
+				return x1 + this._x; // RELATIVE
 			}
 		} else {
-			return this._x;
+			return this._x; // ABSOLUTE
 		}
 	}
 	_class.prototype.y = function(v) {
-		if (v) {
-			if (this._origin) {
-				var y1 = this._origin.y();
-				if (this._origin2) {
-					var w = this._origin2.y() - y1;
-					this._y = (v - y1) / w ;
-				} else {
-					this._y = v - y1 ;
-				}
-			} else {
-				this._y = v;
-			}
-			return this;
-		}
+		if (v) return this.putY(v);
 		
 		var yy = this.calcY();
 		if (yy<0) yy = 0;
 		if (yy>Canvas.height()) yy = Canvas.height;
 		return yy;
 	}
+	_class.prototype.putY = function(v) {
+		if (this._origin) {
+			var y1 = this._origin.y();
+			if (this._origin2) {
+				var y2 = this._origin2.y();
+				var w = y2-y1;
+				var per = (v - y1) / w;
+				console.log(per);
+				if (0.0 <= per && per<= 1.0) {
+					this.mode = 2; // FLOATING-IN-BOUNDS
+					this._y = per;
+				} else {
+					this.mode = 3; // FLOATING-OUT-BOUNDS
+					this._y = v - (y1 + w/2);
+				}
+			} else {
+				this.mode = 1; // RELATIVE
+				this._y = v - y1 ;
+			}
+		} else {
+			this.mode = 0; // ABSOLUTE
+			this._y = v;
+		}
+		return this;
+	}
 	_class.prototype.calcY = function() {
 		if (this._origin) {
 			var y1 = this._origin.y();
 			if (this._origin2) {
-				var w = this._origin2.y() - y1;
-				return y1 + w * this._y;
+				var y2 = this._origin2.y();
+				var w = y2 - y1;
+				var per = this._y;
+				
+				if (0.0 <= per && per <= 1.0) {
+					return y1 + w * per; // FLOATING-IN-BOUNDS
+				} else {
+					return y1 + w/2 + this._y; // FLOATING-OUT-BOUNDS
+				}
 			} else {
-				return y1 + this._y;
+				return y1 + this._y; // RELATIVE
 			}
 		} else {
-			return this._y;
+			return this._y; // ABSOLUTE
 		}
 	}
+	
+	
+	
+	
 	_class.prototype.checkRemove = function() {
 		with (this) {
 			if (_origin && _origin.isRemove) setOrigin(null);
