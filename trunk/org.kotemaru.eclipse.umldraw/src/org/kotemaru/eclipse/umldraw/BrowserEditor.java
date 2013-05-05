c813443360aee84f51f966be8427664164293a5d
kotemaru@kotemaru.org <kotemaru@kotemaru.org@007fbaa9-8b8e-5bb4-69cf-ca3c05551f93>
2013-05-05 08:24:05 +0000

    TODO: clip bord

--- trunk/org.kotemaru.eclipse.umldraw/src/org/kotemaru/eclipse/umldraw/BrowserEditor.java
+++ trunk/org.kotemaru.eclipse.umldraw/src/org/kotemaru/eclipse/umldraw/BrowserEditor.java
@@ -15,7 +15,11 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.StatusTextEvent;
 import org.eclipse.swt.browser.StatusTextListener;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IFileEditorInput;
@@ -23,36 +27,37 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.part.EditorPart;
 
-
-
 public class BrowserEditor extends TextEditor {
 
 	private static final String ENCODING = "utf-8";
-	
+
 	private Browser browser;
+	private Clipboard clipboard;
 	private int changeHistoryCount = 0;
-	private int savedHistoryCount  = 0;
-	
+	private int savedHistoryCount = 0;
+
 	public BrowserEditor() {
 		super();
+
+		Display display = new Display();
+		clipboard = new Clipboard(display);
 	}
-	
+
 	@Override
 	public void init(IEditorSite site, IEditorInput input)
 			throws PartInitException {
-        setSite(site);
-        setInput(input);
-        setPartName(input.getName());
+		setSite(site);
+		setInput(input);
+		setPartName(input.getName());
 	}
-
 
 	@Override
 	public void createPartControl(Composite parent) {
 		try {
 			browser = new Browser(parent, SWT.NONE);
-			browser.setJavascriptEnabled(true);			
+			browser.setJavascriptEnabled(true);
 			browser.addStatusTextListener(new MyStatusTextListener());
-			
+
 			URL aboutURL = this.getClass().getResource("/webapps/editor.html");
 			URL url = FileLocator.resolve(aboutURL);
 			browser.setUrl(url.toString());
@@ -61,11 +66,11 @@ public class BrowserEditor extends TextEditor {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	private class MyStatusTextListener implements StatusTextListener {
 		@Override
 		public void changed(StatusTextEvent ev) {
-			log("status="+ev.text);
+			log("status=" + ev.text);
 			String[] params = ev.text.split(",");
 			String method = params[0];
 			if ("load".equals(method)) {
@@ -74,6 +79,8 @@ public class BrowserEditor extends TextEditor {
 				onChange(params);
 			} else if ("syncPreferences".equals(method)) {
 				onSyncPreferences(params);
+			} else if ("copyClip".equals(method)) {
+				onCopyClip(params);
 			}
 		}
 
@@ -81,47 +88,62 @@ public class BrowserEditor extends TextEditor {
 
 	private void onLoad(String[] params) {
 		startup();
-		
-		IFileEditorInput input = (IFileEditorInput)getEditorInput();
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();  
-		File workspaceDirectory = workspace.getRoot().getLocation().toFile(); 
-		String path = input.getFile().getFullPath().toString().replaceFirst("^/","");
-		File file = new File(workspaceDirectory,path);
-		
-		String url = "file:///"+file.getAbsolutePath().replaceAll("[\\\\]","/");
-		browser.execute("Eclipse.setContentUrl('"+url+"')");
+
+		IFileEditorInput input = (IFileEditorInput) getEditorInput();
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		File workspaceDirectory = workspace.getRoot().getLocation().toFile();
+		String path = input.getFile().getFullPath().toString().replaceFirst("^/", "");
+		File file = new File(workspaceDirectory, path);
+
+		String url = "file:///" + file.getAbsolutePath().replaceAll("[\\\\]", "/");
+		browser.execute("Eclipse.setContentUrl('" + url + "')");
 	}
+
 	private void startup() {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 		String json = "";
-		for (int i=0; i<Preference.ITEMS.length; i++) {
+		for (int i = 0; i < Preference.ITEMS.length; i++) {
 			String key = Preference.ITEMS[i].key;
 			if (Preference.ITEMS[i].type == String.class) {
-				json += key+":'"+store.getString(key)+"',";
+				json += key + ":'" + store.getString(key) + "',";
 			} else {
-				json += key+":"+store.getString(key)+",";
+				json += key + ":" + store.getString(key) + ",";
 			}
 		}
-		
-		browser.execute("Eclipse.startup({"+json+"})");
+
+		browser.execute("Eclipse.startup({" + json + "})");
 	}
-	
+
 	private void onSyncPreferences(String[] params) {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-		for (int i=0; i<Preference.ITEMS.length; i++) {
+		for (int i = 0; i < Preference.ITEMS.length; i++) {
 			String key = Preference.ITEMS[i].key;
-			String val = (String) browser.evaluate("return Eclipse.getPreferences('"+key+"')");
+			String val = (String) browser.evaluate("return Eclipse.getPreferences('" + key + "')");
 			store.setValue(key, val);
 		}
 		try {
-			((IPersistentPreferenceStore)store).save();
+			((IPersistentPreferenceStore) store).save();
 		} catch (Exception e) {
-			log("Error:"+e);
+			log("Error:" + e);
 		}
 	}
+
 	private void onChange(String[] params) {
 		changeHistoryCount = Integer.valueOf(params[1]);
-		firePropertyChange(EditorPart.PROP_DIRTY); 
+		firePropertyChange(EditorPart.PROP_DIRTY);
+	}
+	
+	private void onCopyClip(String[] params) {
+		String textData = (String) browser.evaluate("return Eclipse.getCopyClip();");
+		TextTransfer textTransfer = TextTransfer.getInstance();
+		clipboard.setContents(new Object[] { textData },
+				new Transfer[] { textTransfer });
+	}
+
+	// TODO:
+	public void onPasteClip(String[] params) {
+		TextTransfer transfer = TextTransfer.getInstance();
+		String data = (String) clipboard.getContents(transfer);
 	}
 
 	@Override
@@ -130,12 +152,12 @@ public class BrowserEditor extends TextEditor {
 		try {
 			IFile file = ((IFileEditorInput) getEditorInput()).getFile();
 			file.setContents(
-				new ByteArrayInputStream(content.getBytes(ENCODING)),
-				true,  // keep saving, even if IFile is out of sync with the Workspace
-				false, // dont keep history
-				monitor); // progress monitor
+					new ByteArrayInputStream(content.getBytes(ENCODING)),
+					true, // keep saving, even if IFile is out of sync with the Workspace
+					false, // dont keep history
+					monitor); // progress monitor
 			savedHistoryCount = changeHistoryCount;
-			firePropertyChange(EditorPart.PROP_DIRTY); 
+			firePropertyChange(EditorPart.PROP_DIRTY);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
@@ -147,7 +169,6 @@ public class BrowserEditor extends TextEditor {
 		return savedHistoryCount != changeHistoryCount;
 	}
 
-
 	@Override
 	public void dispose() {
 		browser.dispose();
@@ -157,15 +178,17 @@ public class BrowserEditor extends TextEditor {
 	@Override
 	public void setFocus() {
 	}
+
 	@Override
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
+
 	@Override
 	public void doSaveAs() {
 		// Unsupported
 	}
-	
+
 	private void log(String msg) {
 		System.out.println(msg); // TODO:
 	}
